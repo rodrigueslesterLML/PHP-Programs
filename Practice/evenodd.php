9d84e716850caafdc030d71870a9fe00bc528111
rodrigueslesterLML <rodrigueslester2001@gmail.com>
2021-09-25 09:33:43 +0530

    Added Even Odd Prog

---
+++ Practice/evenodd.php
@@ -0,0 +1,12 @@
+<?php   
+    if($_POST){  
+        $number = $_POST['number'];   
+        //divide entered number by 2   
+        //if the reminder is 0 then the number is even otherwise the number is odd  
+        if(($number % 2) == 0){  
+            echo "$number is an Even number";  
+        }else{  
+            echo "$number is Odd number";  
+        }  
+    }  
+?>
